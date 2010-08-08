2617c35a109f4b943210ed8bbaa57e0ffe2ad586
Haig Papaghanian <turntayble81@gmail.com>
2010-08-07 20:02:09 -0400

    Renamed database tables to fit new system design.

--- api/classes/system.inc.php
+++ api/classes/system.inc.php
@@ -9,10 +9,10 @@
         }
 
         public function newKit($name) {
-            if($this->db->exec("INSERT INTO system_sound_kit (name) VALUES('$name')")) {
+            if($this->db->exec("INSERT INTO sound_kits (name) VALUES('$name')")) {
                 $id = $this->db->lastInsertRowID();
                 for($n=0; $n<MAX_CHANNELS; $n++) {
-                    $this->db->exec("INSERT INTO system_sound_kit_channel (id, channel) VALUES($id, $n)");
+                    $this->db->exec("INSERT INTO sound_kit_channels (id, channel) VALUES($id, $n)");
                 }
                 return true;
             }else {
@@ -21,8 +21,8 @@
         }
 
         public function deleteKit($id) {
-            if($this->db->exec("DELETE FROM system_sound_kit WHERE id=" . $id)) {
-                $this->db->exec("DELETE FROM system_sound_kit_channel WHERE id=" . $id);
+            if($this->db->exec("DELETE FROM sound_kits WHERE id=" . $id)) {
+                $this->db->exec("DELETE FROM sound_kit_channels WHERE id=" . $id);
                 return true;
             }else {
                 return false;
@@ -30,7 +30,7 @@
         }
 
         public function getKits() {
-            $result = $this->db->query("SELECT id, name from system_sound_kit WHERE user_id=" . $this->adminId . " ORDER BY name");
+            $result = $this->db->query("SELECT id, name from sound_kits WHERE user_id=" . $this->adminId . " ORDER BY name");
             return $this->_getAllRows($result);
         }
         
@@ -46,12 +46,12 @@
                 $format = "mp3, ogg";
             }            
 
-            $result = $this->db->query("SELECT name, channel, " . $format . " FROM system_sound_kit_channel WHERE id=" . $id . " ORDER BY channel ASC");
+            $result = $this->db->query("SELECT name, channel, " . $format . " FROM sound_kit_channels WHERE id=" . $id . " ORDER BY channel ASC");
             return $this->_getAllRows($result);
         }
         
         public function updateKitChannel($id, $channel, $name, $ogg, $mp3) {
-            if($this->db->exec("UPDATE system_sound_kit_channel SET name='$name', ogg='$ogg', mp3='$mp3' WHERE id=$id AND channel=$channel")) {
+            if($this->db->exec("UPDATE sound_kit_channels SET name='$name', ogg='$ogg', mp3='$mp3' WHERE id=$id AND channel=$channel")) {
                 return true;
             }else {
                 return false;
