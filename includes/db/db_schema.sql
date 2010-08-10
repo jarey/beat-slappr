5edfd9c131d7f7dcc59b0e1857d85a0b1efbaa86
Haig Papaghanian <turntayble81@gmail.com>
2010-08-09 20:09:18 -0400

    Added MySQL db schema and sampledata SQL files to the includes/db folder in preparation for migration from SQLite to MySQL.

---
+++ includes/db/db_schema.sql
@@ -0,0 +1,54 @@
+/******************************************/
+/***CREATE DATABASE IF IT DOES NOT EXIST***/
+/******************************************/
+
+create database if not exists beat_slappr;
+use beat_slappr;
+
+
+/*******************/
+/***CREATE TABLES***/
+/*******************/
+
+/******************************************
+    USERS TABLE
+    STORES ALL USERS OF THE APPLICATION
+******************************************/
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+
+/******************************************
+    SOUND_KITS TABLE
+    STORES ALL SOUND KIT DEFIINITIONS
+******************************************/
+
+DROP TABLE IF EXISTS `sound_kits`;
+CREATE TABLE IF NOT EXISTS `sound_kits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+
+/**************************************************
+    SOUND_KITS_CHANNELS TABLE
+    STORES ALL SOUND KIT CHANNEL SOUND DEFIINITIONS
+**************************************************/
+
+DROP TABLE IF EXISTS `sound_kit_channels`;
+CREATE TABLE IF NOT EXISTS `sound_kit_channels` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `channel` int(11) NOT NULL,
+  `ogg` mediumtext NOT NULL,
+  `mp3` mediumtext NOT NULL,
+  UNIQUE KEY `id` (`id`,`channel`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
