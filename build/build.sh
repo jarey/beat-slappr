974c4005589f95c239844854358bcbbf1306eb61
Haig Papaghanian <turntayble81@gmail.com>
2010-09-09 15:14:39 -0400

    Updated build script package naming again.  This time: date - hash - build.  Easier to read.

--- build/build.sh
+++ build/build.sh
@@ -118,7 +118,7 @@ do
 
     mv config/config.$build.php config.php
     curDate=`date +%Y_%m_%d_%H_%M_%S`
-    tar czf ../packages/build__`echo $curDate`__`echo $build`__`echo $gitHash`.tar.gz * --exclude "config"
+    tar czf ../packages/build__`echo $curDate`__`echo $gitHash`__`echo $build`.tar.gz * --exclude "config"
     rm config.php
 done
 
