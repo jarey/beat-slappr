ca7b248aa05bcb5412576d96d01a6ad5918199da
Haig Papaghanian <turntayble81@gmail.com>
2010-09-18 16:07:56 -0400

    Updated kit sample sync functionality to convert mono samples to stereo IF one of the existing samples in the kit is stereo.  This will ensure proper mixdowns.

--- admin/kits/index.php
+++ admin/kits/index.php
@@ -19,12 +19,24 @@
                 $kitId = $val['id'];
                 $kitChannels = $kitAPI->getKitChannels($kitId, 'ogg');
                 mkdir($downloadDir . $kitId);
+                $channelArr = array();
                 foreach($kitChannels as $key => $val) {
                     $fileName = $downloadDir . $kitId . '/' . $val['channel'] . '.ogg';
                     $fp = fopen($fileName, 'wb');
                     fwrite($fp, base64_decode($val['src']));
                     fclose($fp);
-                    //$channels = exec("soxi -c " . $fileName);
+                    $channel = exec("soxi -c " . $fileName);
+                    array_push($channelArr, $channel);
+                }
+
+                $maxChannels = max($channelArr);
+                if($maxChannels > 1) {
+                    foreach($channelArr as $key => $val) {
+                        if($val ==1) {
+                            $fileName = $downloadDir . $kitId . '/' . $key . ".ogg";
+                            exec("sox " . $fileName . " " . $fileName . " remix 1 1");
+                        }
+                    }
                 }
             }
         }
