6961bb926ee671573bc91ba272afed90ff3e8293
Haig Papaghanian <turntayble81@gmail.com>
2011-02-15 14:53:08 -0500

    Added analytics code to account pages (Used for activating accounts and resetting passwords).

--- account/includes/templates/main.tpl.php
+++ account/includes/templates/main.tpl.php
@@ -12,7 +12,33 @@
             <html>
                 <head>
                     <title>" . $data['title'] . "</title>
-                    <link rel='stylesheet' href='" . APP_URL . "account/includes/style/style.css' type='text/css' media='screen' />
+                    <link rel='stylesheet' href='" . APP_URL . "account/includes/style/style.css' type='text/css' media='screen' />";
+
+                    if(!DEV) { ?>
+                        <script type="text/javascript">
+                            var _gaq = _gaq || [];
+                            _gaq.push(['_setAccount', 'UA-3259969-2']);
+                            _gaq.push(['_trackPageview']);
+
+                            (function() {
+                                var  ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                            })();
+
+                            var htA=htA||[],
+                                hURL=hURL||"//<?php echo $_SERVER["SERVER_NAME"]; ?>/hotlinks/";
+                            htA.push(['a','12345']);
+                            (function(){
+                                var s,d;
+                                s=document.createElement('script');s.type='text/javascript';s.async=true;
+                                s.src=document.location.protocol+hURL+"tracker.js";
+                                d=document.getElementsByTagName('script')[0];d.parentNode.insertBefore(s, d);
+                            })();
+                        </script>
+                    <?php }
+
+                echo "
                 </head>
                 <body>
                     <div id='divHeader'>
